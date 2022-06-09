4b281d99c1cb5f47ed2c3a7aa4cb75f78a6f736a
immoghul <moghul02@gmail.com>
2022-06-09 14:43:54 -0400

    retrieves from API

--- app/src/main/java/com/example/salahreader/SettingsActivity.java
+++ app/src/main/java/com/example/salahreader/SettingsActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -30,8 +35,31 @@ public class SettingsActivity extends AppCompatActivity {
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            GetSalah.getDataStr();
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            GetData g = new GetData();
+            Thread thread = new Thread(g);
+            thread.start();
+//            thread.join();
+            String d = g.getValue();
+//            // Load the preferences from an XML resource
+//            addPreferencesFromResource(R.xml.root_preferences);
+//            //Place after here
+//            EditTextPreference[] myTextView = new EditTextPreference[10];
+//            myTextView[0] = (EditTextPreference) findPreference("fajrTime");
+//            myTextView[0].setTitle(data);
         }
+    }
+}
+
+class GetData implements Runnable {
+    private volatile String value = "";
+
+    @Override
+    public void run() {
+        value = GetSalah.getDataStr();
+    }
+
+    public String getValue() {
+        return value;
     }
 }

a3a9b38d2894b3c5d008e2f400f9cf146ed3b121
immoghul <moghul02@gmail.com>
2022-06-10 18:39:22 -0400

    added refresh button

--- app/src/main/java/com/example/salahreader/SettingsActivity.java
+++ app/src/main/java/com/example/salahreader/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,19 +24,21 @@ import androidx.preference.PreferenceFragmentCompat;
 public class SettingsActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     public final String[] DropdownOptions = {"Refresh"};
+    public SettingsFragment sf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sf = new SettingsFragment();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
             .beginTransaction()
-            .replace(R.id.settings, new SettingsFragment())
+            .replace(R.id.settings, sf)
             .commit();
         }
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, DropdownOptions);
+
 
         ActionBar actionBar = getSupportActionBar();
 
@@ -42,7 +46,27 @@ public class SettingsActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(false);
             actionBar.setDisplayShowHomeEnabled(true);
         }
-        
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.refreshButton:
+                RetrieveAPI.refresh();
+                if(sf!=null) sf.updateTimes();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -52,8 +76,12 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
             // update and retrive values from API
             RetrieveAPI.refresh();
-            String[] todaysTimes = RetrieveAPI.getCurrentTimes();
             // update text prefernces
+            updateTimes();
+        }
+
+        public void updateTimesTextView(){
+            String[] todaysTimes = RetrieveAPI.getCurrentTimes();
             EditTextPreference myTextView;
             String[] keys = {"fajrTime","thuhrTime","asrTime","magribTime","ishaaTime"};
             for(int i = 0;i<5;++i){
@@ -62,6 +90,9 @@ public class SettingsActivity extends AppCompatActivity {
                 myTextView.setText(todaysTimes[i]);
                 myTextView.setEnabled(false);
             }
+        }
+        public void updateTimes(){
+            updateTimesTextView();
         }
     }
 }
